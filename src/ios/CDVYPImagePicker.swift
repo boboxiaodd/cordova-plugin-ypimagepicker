20bb0f01fe645f7270c5d8e4843403b09865af26
bobo <iam386@gmail.com>
2019-10-26 09:50:49 +0800

    first commit

--- src/ios/CDVYPImagePicker.swift
+++ src/ios/CDVYPImagePicker.swift
@@ -1,18 +1,72 @@
+import Foundation
+import YPImagePicker
+import AVFoundation
+import UIKit
 /*
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
 @objc(CDVYPImagePicker) class CDVYPImagePicker : CDVPlugin {
-  @objc(yourFunctionName:) // Declare your function name.
-  func yourFunctionName(command: CDVInvokedUrlCommand) { // write the function code.
-    /*
-     * Always assume that the plugin will fail.
-     * Even if in this example, it can't.
-     */
-    // Set the plugin result to fail.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "The Plugin Failed");
-    // Set the plugin result to succeed.
-    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "The plugin succeeded");
-    // Send the function result back to Cordova.
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+  @objc(open:) // Declare your function name.
+  func open(command: CDVInvokedUrlCommand) { // write the function code.
+    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "picker type error")
+    var config = YPImagePickerConfiguration()
+    let picker_type = command.argument(at: 0)
+    switch(picker_type as! String ){
+        case "video":
+            config.startOnScreen = YPPickerScreen.library
+            config.library.mediaType = .video
+            config.screens = [.library, .photo]
+            config.video.compression = AVAssetExportPresetHighestQuality
+            config.video.fileType = .mp4
+            config.showsCrop = .none
+            config.showsVideoTrimmer = false
+            config.isScrollToChangeModesEnabled = false
+            let picker = YPImagePicker(configuration: config)
+            picker.didFinishPicking { [unowned picker] items, _ in
+                if let video = items.singleVideo {
+                    let json = ["url": video.url.path] as [AnyHashable : Any]
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
+                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                }
+                picker.dismiss(animated: true, completion: nil)
+            }
+            self.viewController.present(picker, animated: true)
+            break;
+        case "avatar":
+            config.startOnScreen = YPPickerScreen.library
+            config.library.mediaType = .photo
+            config.screens = [.library, .photo]
+            config.showsCrop = .rectangle(ratio: 1)
+            config.targetImageSize = .cappedTo(size: 500)
+            config.isScrollToChangeModesEnabled = false
+            let picker = YPImagePicker(configuration: config)
+            picker.didFinishPicking { [unowned picker] items, _ in
+                if let photo = items.singleVideo {
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["url":photo.url])
+                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                }
+                picker.dismiss(animated: true, completion: nil)
+            }
+            self.viewController.present(picker, animated: true)
+            break;
+        case "photo":
+            config.startOnScreen = YPPickerScreen.library
+            config.library.mediaType = .photo
+            config.screens = [.library, .photo]
+            config.showsCrop = .none
+            config.isScrollToChangeModesEnabled = false
+            let picker = YPImagePicker(configuration: config)
+            picker.didFinishPicking { [unowned picker] items, _ in
+                if let photo = items.singleVideo {
+                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["url":photo.url])
+                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                }
+                picker.dismiss(animated: true, completion: nil)
+            }
+            self.viewController.present(picker, animated: true)
+            break;
+        default:
+            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+    }
   }
 }
--- www/CDVYPImagePicker.js
+++ www/CDVYPImagePicker.js
@@ -5,7 +5,7 @@ var PLUGIN_NAME = "CDVYPImagePicker"; // This is just for code completion uses.
 var CDVYPImagePicker = function() {}; // This just makes it easier for us to export all of the functions at once.
 // All of your plugin functions go below this.
 // Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-CDVYPImagePicker.yourFunctionName = function(onSuccess, onError) {
-    exec(onSuccess, onError, PLUGIN_NAME, "open", []);
+CDVYPImagePicker.open = function(onSuccess, onError , type) {
+    exec(onSuccess, onError, PLUGIN_NAME, "open", [type]);
 };
 module.exports = CDVYPImagePicker;
