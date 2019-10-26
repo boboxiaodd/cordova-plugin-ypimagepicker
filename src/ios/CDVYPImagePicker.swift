170f756eb8545ac2d100a928db113219da495693
bobo <iam386@gmail.com>
2019-10-26 13:08:04 +0800

    first commit

--- src/ios/CDVYPImagePicker.swift
+++ src/ios/CDVYPImagePicker.swift
@@ -6,67 +6,92 @@ import UIKit
 * Notes: The @objc shows that this class & function should be exposed to Cordova.
 */
 @objc(CDVYPImagePicker) class CDVYPImagePicker : CDVPlugin {
-  @objc(open:) // Declare your function name.
-  func open(command: CDVInvokedUrlCommand) { // write the function code.
-    var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "picker type error")
-    var config = YPImagePickerConfiguration()
-    let picker_type = command.argument(at: 0)
-    switch(picker_type as! String ){
-        case "video":
-            config.startOnScreen = YPPickerScreen.library
-            config.library.mediaType = .video
-            config.screens = [.library, .photo]
-            config.video.compression = AVAssetExportPresetHighestQuality
-            config.video.fileType = .mp4
-            config.showsCrop = .none
-            config.showsVideoTrimmer = false
-            config.isScrollToChangeModesEnabled = false
-            let picker = YPImagePicker(configuration: config)
-            picker.didFinishPicking { [unowned picker] items, _ in
-                if let video = items.singleVideo {
-                    let json = ["url": video.url.path] as [AnyHashable : Any]
-                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
-                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+    @objc(open:) // Declare your function name.
+    func open(command: CDVInvokedUrlCommand) { // write the function code.
+        var pluginResult = CDVPluginResult (status: CDVCommandStatus_ERROR, messageAs: "picker type error")
+        var config = YPImagePickerConfiguration()
+        config.library.onlySquare = false
+        let arg = command.argument(at: 0) as! [AnyHashable : Any]
+        switch(arg["type"] as! String ){
+            case "video":
+                config.startOnScreen = YPPickerScreen.library
+                config.library.mediaType = .video
+                config.screens = [.library, .photo]
+                config.video.fileType = .mp4
+                config.showsCrop = .none
+                config.showsVideoTrimmer = false
+                config.isScrollToChangeModesEnabled = false
+                let picker = YPImagePicker(configuration: config)
+                picker.didFinishPicking { [unowned picker] items, _ in
+                    if let video = items.singleVideo {
+                        let videoInfo = try! video.url.resourceValues(forKeys: [.fileSizeKey])
+                        let json = ["url": video.url.absoluteString,
+                                    "size": videoInfo.fileSize ?? 0,
+                                    "duration":video.asset?.duration.description ?? "0"] as [AnyHashable : Any]
+                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
+                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                    }
+                    picker.dismiss(animated: true, completion: nil)
                 }
-                picker.dismiss(animated: true, completion: nil)
-            }
-            self.viewController.present(picker, animated: true)
-            break;
-        case "avatar":
-            config.startOnScreen = YPPickerScreen.library
-            config.library.mediaType = .photo
-            config.screens = [.library, .photo]
-            config.showsCrop = .rectangle(ratio: 1)
-            config.targetImageSize = .cappedTo(size: 500)
-            config.isScrollToChangeModesEnabled = false
-            let picker = YPImagePicker(configuration: config)
-            picker.didFinishPicking { [unowned picker] items, _ in
-                if let photo = items.singleVideo {
-                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["url":photo.url])
-                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                self.viewController.present(picker, animated: true)
+                break;
+            case "avatar":
+                config.startOnScreen = YPPickerScreen.library
+                config.library.mediaType = .photo
+                config.screens = [.library, .photo]
+                config.showsCrop = .rectangle(ratio: 1)
+                config.showsPhotoFilters = arg["filter"] as? Bool ?? false
+                config.targetImageSize = .cappedTo(size: CGFloat(arg["size"] as? Int ?? 500))
+                config.isScrollToChangeModesEnabled = false
+                config.shouldSaveNewPicturesToAlbum = false
+                let picker = YPImagePicker(configuration: config)
+                picker.didFinishPicking { [unowned picker] items, _ in
+                    if let photo = items.singlePhoto {
+                        let fileInfo = self.saveImage(image: photo.image)
+                        let json = ["url": fileInfo["path"] ?? "",
+                                    "size": fileInfo["fileSize"] ?? 0
+                                   ] as [AnyHashable : Any]
+                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
+                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                    }
+                    picker.dismiss(animated: true, completion: nil)
                 }
-                picker.dismiss(animated: true, completion: nil)
-            }
-            self.viewController.present(picker, animated: true)
-            break;
-        case "photo":
-            config.startOnScreen = YPPickerScreen.library
-            config.library.mediaType = .photo
-            config.screens = [.library, .photo]
-            config.showsCrop = .none
-            config.isScrollToChangeModesEnabled = false
-            let picker = YPImagePicker(configuration: config)
-            picker.didFinishPicking { [unowned picker] items, _ in
-                if let photo = items.singleVideo {
-                    pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: ["url":photo.url])
-                    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                self.viewController.present(picker, animated: true)
+                break;
+            case "photo":
+                config.startOnScreen = YPPickerScreen.library
+                config.library.mediaType = .photo
+                config.screens = [.library, .photo]
+                config.showsCrop = .none
+                config.targetImageSize = .cappedTo(size: CGFloat(arg["size"] as? Int ?? 1024))
+                config.isScrollToChangeModesEnabled = false
+                config.showsPhotoFilters = arg["filter"] as? Bool ?? false
+                config.shouldSaveNewPicturesToAlbum = false
+                let picker = YPImagePicker(configuration: config)
+                picker.didFinishPicking { [unowned picker] items, _ in
+                    if let photo = items.singlePhoto {
+                        let fileInfo = self.saveImage(image: photo.image)
+                        let json = ["url": fileInfo["path"] ?? "",
+                                    "size": fileInfo["size"] ?? 0,
+                                    "width": photo.image.size.width,
+                                    "height": photo.image.size.height
+                                   ] as [AnyHashable : Any]
+                        pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: json)
+                        self.commandDelegate!.send(pluginResult, callbackId: command.callbackId);
+                    }
+                    picker.dismiss(animated: true, completion: nil)
                 }
-                picker.dismiss(animated: true, completion: nil)
-            }
-            self.viewController.present(picker, animated: true)
-            break;
-        default:
-            self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+                self.viewController.present(picker, animated: true)
+                break;
+            default:
+                self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+        }
     }
-  }
+    func saveImage(image:UIImage) -> [AnyHashable : Any] {
+        let tmpDirectory = FileManager.default.temporaryDirectory;
+        let path = tmpDirectory.appendingPathComponent("\(UUID().uuidString).jpg")
+        try! image.jpegData(compressionQuality: 0.8)?.write(to: path)
+        let fileInfo = try! path.resourceValues(forKeys: [.fileSizeKey])
+        return ["path":path.absoluteString,"size":fileInfo.fileSize ?? 0]
+    }
 }
--- www/CDVYPImagePicker.js
+++ www/CDVYPImagePicker.js
@@ -3,9 +3,25 @@ var exec = require('cordova/exec');
 var PLUGIN_NAME = "CDVYPImagePicker"; // This is just for code completion uses.
 
 var CDVYPImagePicker = function() {}; // This just makes it easier for us to export all of the functions at once.
-// All of your plugin functions go below this.
-// Note: We are not passing any options in the [] block for this, so make sure you include the empty [] block.
-CDVYPImagePicker.open = function(onSuccess, onError , type) {
-    exec(onSuccess, onError, PLUGIN_NAME, "open", [type]);
+/*
+options:
+ type: photo , video , avatar
+ size: Integer , max photo width or height
+ filter: Bool , enable/disable show filter
+
+return:
+  photo / avatar:
+    url:   //tmpDirectory file with jpg file type
+    width: //photo's width
+    height: //photo's height
+    size: //file size
+
+  video:
+    url: //tmpDirectory file with mp4 file type
+    duration: //video duration
+    size: //file size
+*/
+CDVYPImagePicker.open = function(onSuccess, onError , options) {
+    exec(onSuccess, onError, PLUGIN_NAME, "open", [options]);
 };
 module.exports = CDVYPImagePicker;
